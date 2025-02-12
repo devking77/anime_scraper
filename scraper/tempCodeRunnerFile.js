d78e065b6065288325de86a3c66a300c732a7deb
Devender Saini <devs97111@gmail.com>
2025-02-12 12:13:17 +0530

    test commit

--- scraper/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-// async getServer() {
-    
-//     const {data}=await axios.get(url,{"headers":headers})
-//     const html=cheer.load(data)
-//     // console.log(data)
-    
-    
-// }
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-on = match[1];
-        const uri = match[2];
-        console.log("Resolution:", resolution);
-        console.log("UR
